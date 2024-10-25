2940647d626976f09163cb752bb051d565ce5b2d
Elizabeth <122000137+Elizabeth-GitHub@users.noreply.github.com>
2024-10-25 12:10:27 +0800

    Implement displayBook function
    - Created the `displayBook`` function to append a
    list item for each book to the book list.
    - Integrated `displayBook` into the
    `addBookToLibrary` function to show books in the
    list upon addition to the library.
    - Added sample books for demonstration purposes.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const bodyContainer = document.getElementById('body-container');
+const mainHeaderContainer = document.getElementById('mainheader-container');
+const bookListContainer = document.getElementById('booklist-container');
+const bookList = document.getElementById('booklist');
+const newBookButtonContainer = document.getElementById('newbookbutton-container');
+const newBookButton = document.getElementById('newbook-button');
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -7,10 +14,24 @@ function Book(title, author, pages, read) {
     this.read = read; // Boolean value (true or false)
     this.info = function() {
         const readStatus = this.read ? 'Read' : 'Not Read';
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus}`;}
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;}
+
+    addBookToLibrary(this.info());
 }
 
 
 function addBookToLibrary(bookToAdd) {
     myLibrary.push(bookToAdd);
+    displayBook(bookToAdd);
+}
+
+const Hobbit = new Book('Hobbit', 'Tolkien', 1000, false);
+console.log(Hobbit.info());
+console.log(myLibrary);
+
+function displayBook(bookToDisplay) {
+    let newBookInList = document.createElement('li');
+
+    newBookInList.textContent = bookToDisplay;
+    bookList.appendChild(newBookInList);
 }
